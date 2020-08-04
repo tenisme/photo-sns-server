4de73a101709621f1dd350080339f90d6c1c222a
tenisme <iamchoma@gmail.com>
2020-08-04 12:57:31 +0900

    add

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5700;
 
+app.get("/", (req, res, next) => {
+  res.json({ success: true });
+});
+
 app.listen(PORT, () => {
   console.log("App Listening on port 5700");
 });
