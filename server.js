ff9be03036dac2d5ea7c45dddb5c39980ae7285b
tenisme <iamchoma@gmail.com>
2020-08-04 12:09:14 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5700;
+
+app.listen(PORT, () => {
+  console.log("App Listening on port 5700");
+});
